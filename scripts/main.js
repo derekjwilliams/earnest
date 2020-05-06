d5045df568fdaefcb466277d6b9f50f8a451a58b
Derek Williams <derek61@gmail.com>
2020-05-06 10:09:42 -0600

    graphql example

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,36 @@
-function setItems(data) {
-    if (data.length === 0) {
-        return false;
-    }
+function setLocations(value) {
+  const locationNodes = value.data.allLocations.nodes
 
-    const grid = document.getElementById('language');
-    data.forEach(item => {
-        //get data and create a list of phases
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <div class="lang-grid--item">
-                <h2 class="lang-grid--title">${item.phase}</h2>
-                <p class="lang-grid--trans">${item.trans}</p>
-            </div>
-        `;
-        grid.appendChild(div);
-    })
+  locationNodes.forEach(node => {
+      const div = document.createElement('div')
+      div.innerHTML = `
+          <div class="location">
+              <div class="province-state">${node.provinceState}</div>
+              <div class="country-region">${node.countryRegion}</div>
+          </div>
+      `
+      document.querySelector('.locations').appendChild(div)
+  })
 }
 
-const getItem = () => fetch("./language.json")
-        .then(res => res.json())
-        .then(json => {
-            setItems(json);
-            return json;
-        })
-        .catch(err => document.querySelector('.lang-grid__nodata').classList.remove('hide'));
+const getLocationsCount = (count, after) => 
+fetch("http://localhost:5000/graphql", {
+  "headers": {
+    "accept": "application/json",
+    "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+    "content-type": "application/json",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin"
+  },
+  "referrer": "http://localhost:5000/graphiql",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": "{\"query\":\"{\\n  allLocations (first: " + count + ") {\\n    nodes {\\n      provinceState\\n      countryRegion\\n    }\\n  }\\n}\\n\",\"variables\":{\"projectReference\":{\"projectReference\":{\"projectId\":\"3\"}}}}",
+  "method": "POST",
+  "mode": "cors"
+}).then(response => response.json()).then(json => setLocations(json))
 
-const triggerItem = () => {
+const moreLocations = () => {
     const list = document.querySelectorAll('.lang-grid--trans');
     list.forEach(function (item) {
         if (item.classList.contains('hide')) {
@@ -35,9 +40,8 @@ const triggerItem = () => {
         }
     })
 }
-
-getItem();
-document.getElementById('disable').addEventListener('click', triggerItem);
+getLocationsCount(20)
+document.querySelector('.getmore').addEventListener('click', moreLocations);
 
 if (typeof exports !== 'undefined') {
     module.exports = {
