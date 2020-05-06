6b4b766838446d5bb331ad8095b7d7c4c2fd7fca
Derek Williams <derek61@gmail.com>
2020-05-06 11:05:13 -0600

    add jest-fetch-mock for mocking API responses

--- scripts/main.js
+++ scripts/main.js
@@ -13,49 +13,42 @@ function setLocations(value) {
       document.querySelector('.locations').appendChild(div)
   })
 }
-const getLocations = (count, after) => 
-  fetch("http://localhost:8080/tests/exampleResponse.json", {"mode": "no-cors"})
-    .then(response => response.json())
-    .then(json => setLocations(json))
 
-
-// const getLocations = (count, after) => {
-//   const query = "{\"query\":\"{\\n  allLocations (first: " + count + ") {\\n    nodes {\\n      provinceState\\n      countryRegion\\n    }\\n  }\\n}\\n\",\"variables\":{\"projectReference\":{\"projectReference\":{\"projectId\":\"3\"}}}}"
-//   fetch("http://localhost:5000/graphql", {
-//     "headers": {
-//       "accept": "application/json",
-//       "accept-language": "en-US,en;q=0.9,fr;q=0.8",
-//       "content-type": "application/json",
-//       "sec-fetch-dest": "empty",
-//       "sec-fetch-mode": "cors",
-//       "sec-fetch-site": "same-origin"
-//     },
-//     "referrer": "http://localhost:5000/graphiql",
-//     "referrerPolicy": "no-referrer-when-downgrade",
-//     "body": query,
-//     "method": "POST",
-//     "mode": "cors"
-//   }).then(response => response.json())
+// const getLocations = (count, after) => 
+//   fetch("http://localhost:8080/tests/exampleResponse.json", {"mode": "no-cors"})
+//     .then(response => response.json())
 //     .then(json => setLocations(json))
-// }
+
+
+const getLocations = (count, after) => {
+  const query = "{\"query\":\"{\\n  allLocations (first: " + count + ") {\\n    nodes {\\n      provinceState\\n      countryRegion\\n    }\\n  }\\n}\\n\",\"variables\":{\"projectReference\":{\"projectReference\":{\"projectId\":\"3\"}}}}"
+  fetch("http://localhost:5000/graphql", {
+    "headers": {
+      "accept": "application/json",
+      "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+      "content-type": "application/json",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin"
+    },
+    "referrer": "http://localhost:5000/graphiql",
+    "referrerPolicy": "no-referrer-when-downgrade",
+    "body": query,
+    "method": "POST",
+    "mode": "cors"
+  }).then(response => response.json())
+    .then(json => setLocations(json))
+}
 
 const moreLocations = () => {
-    const list = document.querySelectorAll('.lang-grid--trans');
-    list.forEach(function (item) {
-        if (item.classList.contains('hide')) {
-            item.classList.remove('hide');
-        } else {
-            item.classList.add('hide');
-        }
-    })
 }
+
 getLocations(20)
-document.querySelector('.getmore').addEventListener('click', moreLocations);
+document.querySelector('.getmore').addEventListener('click', moreLocations)
 
 if (typeof exports !== 'undefined') {
-    module.exports = {
-        getItem,
-        setItems,
-        triggerItem
-    };
+  module.exports = {
+    moreLocations,
+    getLocations
+  }
 }
--- tests/simplepage.spec.js
+++ tests/simplepage.spec.js
@@ -1,23 +1,34 @@
 import fs from 'fs'
 import path from 'path'
 import '@testing-library/jest-dom'
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
 
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
 
-jest
-    .dontMock('fs');
+jest.dontMock('fs');
+
 
 describe('button', function () {
-    beforeEach(() => {
-        document.documentElement.innerHTML = html.toString();
-    });
+  beforeEach(() => {
+    fetchMock.mockIf(/^https?:\/\/localhost*$/, req => {
+      if (req.url.endsWith("graphql")) {
+        return "some response body"
+      } else {
+        return {
+          status: 404,
+          body: "Not Found"
+        }
+      }
+  })
+  document.documentElement.innerHTML = html.toString()
+})
 
-    afterEach(() => {
-        // restore the original func after test
-        jest.resetModules();
-    });
+  afterEach(() => {
+    jest.resetModules()
+  });
 
-    it('button exists', function () {
-        expect(document.getElementById('disable')).toBeTruthy();
-    });
+  it('button exists', function () {
+    expect(document.querySelector('.getmore')).toBeTruthy()
+  })
 });
